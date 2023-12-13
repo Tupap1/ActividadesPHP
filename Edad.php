641a1729a5ea91440658fef5b457fb4dafbb5556
Tupap1 <126369055+Tupap1@users.noreply.github.com>
2023-12-13 11:09:16 -0500

    Add files via upload

---
+++ Edad.php
@@ -0,0 +1,12 @@
+<?php
+    $edad=62;
+    if ($edad<18){
+        print ("eres menor de edad");
+    }
+    else if ($edad<60){
+        print ("eres mayor de edad");
+    }
+    else {
+        print ("eres un adulto mayor");
+    }
+    ?>
